9b390e13a05adde02d530e6895df16dd214b2cea
Christa Stephens <christastephens@quickenloans.com>
2019-07-18 08:41:36 -0400

    working with toupper and tolower methods

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace basicfunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string firstName = "christa";
+            string lastName = "STEPHENS";
+            firstName = firstName.ToUpper();
+            Console.WriteLine(firstName);
+        }
+    }
+}
