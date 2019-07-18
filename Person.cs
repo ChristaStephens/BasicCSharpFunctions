84c848c0f3aeadc90a3c5dd777d0f68bfd320924
Christa Stephens <christastephens@quickenloans.com>
2019-07-18 09:06:42 -0400

    created person class

---
+++ Person.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Person{
+    public class Person{
+        public string Name;
+        public int Age;
+        public bool HasPet;
+        public void Greeting()
+        {
+            Console.WriteLine("Hey gal! My name is " + Name + " and my age is " + Age);
+
+        }
+    }
+}
